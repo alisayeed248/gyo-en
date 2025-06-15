5fb9d9af3d2884bb1d5ab208f225f91b07f720d3
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-06-15 18:22:03 -0400

    Changed url to urls to check status across different types of URLs.

--- cmd/gyo-en/main.go
+++ cmd/gyo-en/main.go
@@ -7,14 +7,18 @@ import (
 
 func main() {
 	fmt.Println("gyo-en uptime monitor starting...")
-	url := "https://www.google.com"
-	isUp, duration, err := monitor.CheckURL(url)
+	urls := []string{"https://www.google.com", "https://test-fake-website-12443.com", "https://httpstat.us/500",}
 	
-	if err != nil {
+	
+	for _, url := range urls {
+		isUp, duration, err := monitor.CheckURL(url)
+		if err != nil {
 		fmt.Printf("Error checking %s: %v\n", url, err)
 	} else if isUp {
 		fmt.Printf("%s is UP (took %v)\n", url, duration)
 	} else {
 		fmt.Printf("%s is DOWN (took %v)\n", url, duration)
 	}
+	}
+	
 }
