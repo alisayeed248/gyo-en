7b0c714895eb88c4906a08a1b6e0935977addaf7
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-06-19 21:31:52 -0400

    Added monitored URL struct

--- backend/internal/database/models.go
+++ backend/internal/database/models.go
@@ -13,3 +13,12 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
+// monitoredUrl is a URl that a user wants to monitor
+type MonitoredUrl struct {
+	ID uint `json:"id" gorm:"primaryKey"`
+	UserID uint `json:"url" gorm:not null"`
+	URL string `json:"url" gorm:"not null"`
+	Name string `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
