eb370c6eafc886cc069380d001c060e61db4a8b5
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-06-19 21:42:54 -0400

    Fixed an issue with URL and formatted better.

--- backend/internal/database/models.go
+++ backend/internal/database/models.go
@@ -6,28 +6,28 @@ import (
 
 // users are people who sign up
 type User struct {
-	Id	uint 	`json:"id" gorm:"primaryKey"`
-	Username string `json:"username" gorm:"unique;not null"`
-	Email string `json:"email" gorm:"unique;not null"`
-	Password string `json:"-" gorm:"not null"`
+	Id        uint      `json:"id" gorm:"primaryKey"`
+	Username  string    `json:"username" gorm:"unique;not null"`
+	Email     string    `json:"email" gorm:"unique;not null"`
+	Password  string    `json:"-" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
 // monitoredUrl is a URl that a user wants to monitor
 type MonitoredUrl struct {
-	ID uint `json:"id" gorm:"primaryKey"`
-	UserID uint `json:"url" gorm:not null"`
-	URL string `json:"url" gorm:"not null"`
-	Name string `json:"name"`
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	UserID    uint      `json:"user_id" gorm:not null"`
+	URL       string    `json:"url" gorm:"not null"`
+	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
 type CheckResult struct {
-	ID uint `json:"id" gorm:"primaryKey"`
-	URL string `json:"url" gorm:"not null"`
-	IsUp bool `json:"is_up"`
+	ID           uint          `json:"id" gorm:"primaryKey"`
+	URL          string        `json:"url" gorm:"not null"`
+	IsUp         bool          `json:"is_up"`
 	ResponseTime time.Duration `json:"response_time"`
-	StatusCode int `json:"status_code"`
-	ErrorMessage string `json:"error_message"`
-	CheckedAt time.Time `json:"checked_at"`
+	StatusCode   int           `json:"status_code"`
+	ErrorMessage string        `json:"error_message"`
+	CheckedAt    time.Time     `json:"checked_at"`
 }
