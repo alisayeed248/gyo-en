47f9f2323c05f080cfda820ede22d4ecf5bb0f82
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-06-23 20:35:19 -0400

    Got rid of TAILWIND TEST banner and added onClick handler for Login.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 function Navbar() {
+  const handleLoginClick = () => {
+    console.log("Login clicked.")
+  }
+
   return (
     <nav>
-      <div className="bg-yellow-500 text-black text-4xl p-8">TAILWIND TEST</div>
       <div className="fixed top-0 w-full bg-red-500 flex justify-between items-center p-4" style={{right: 0}}>
         <div>gyo-en Monitor</div>
         <div>
-          <button className="mr-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Login</button>
+          <button onClick={handleLoginClick} className="mr-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Login</button>
           <button>Register</button>
         </div>
       </div>
