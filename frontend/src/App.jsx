1f7ffe29f7527099b76da0b89a9c5384d71e55ee
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-06-23 21:31:20 -0400

    Rewrote App.jsx to use routing for current components.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import Dashboard from './components/Dashboard'
-import Navbar from './components/Navbar'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+      {/* We keep Navbar at the top because we want it to show on every page */}
       <Navbar/>
-      <Dashboard/>
-    </div>
-  )
+      {/** The routes allow us to say: based on this path, effect this prop (element) */}
+      <Routes>
+        <Route path="/" element={<Dashboard />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
