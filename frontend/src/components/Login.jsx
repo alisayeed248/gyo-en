de176c9c49eefef402d988097e168a5815a10a5e
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-06-23 22:14:18 -0400

    Added basic login page.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,22 @@
 function Login() {
   return (
     <div>
-      <div className="justify-center">Login</div>
+      <div className="h-screen flex items-center justify-center">
+        <div>
+          <form>
+            <div>Username</div>
+            <input type="text" className="mb-4"></input>
+
+            <div>Password</div>
+            <input type="password" className="mb-4"></input>
+
+            <div>
+              <button type="submit">Login</button>
+            </div>
+            
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
