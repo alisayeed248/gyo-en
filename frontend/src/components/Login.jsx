a1319fa0a1f68659fed3f9b5e5ed08082f935c97
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-06-24 22:09:08 -0400

    Added handler to check for changed fields in form. Mde text black to be visible against form's white background.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,19 +1,33 @@
+import { useState } from "react";
+
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
       <div className="h-screen flex items-center justify-center">
         <div>
           <form>
             <div>Username</div>
-            <input type="text" className="mb-4"></input>
+            <input
+              type="text"
+              className="mb-4 text-black"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            ></input>
 
             <div>Password</div>
-            <input type="password" className="mb-4"></input>
+            <input
+              type="password"
+              className="mb-4 text-black"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
 
             <div>
               <button type="submit">Login</button>
             </div>
-            
           </form>
         </div>
       </div>
