1f9a1697a6168ba3edf1db957e5dd56a37f30f3f
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-06-25 21:26:52 -0400

    Added CORS headers to backend.

--- internal/auth/handlers.go
+++ internal/auth/handlers.go
@@ -4,7 +4,15 @@ import "net/http"
 
 // will take in the request and return a response
 // http.Request contains the POST data (headers, URL, method, etc.)
-// 
+//
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte ("Login endpointt is working."))
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	if r.Method == "OPTIONS" {
+		return
+	}
+	
+	w.Write([]byte("Login endpoint is working."))
 }
