43c8b92132786927fd5102d537e71baa4418f1ec
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-06-25 21:45:44 -0400

    Decoded JSON in POST method.

--- internal/auth/handlers.go
+++ internal/auth/handlers.go
@@ -1,11 +1,20 @@
 package auth
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
 // will take in the request and return a response
 // http.Request contains the POST data (headers, URL, method, etc.)
-//
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	// fixes Cors issue
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
@@ -13,6 +22,16 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "OPTIONS" {
 		return
 	}
-	
-	w.Write([]byte("Login endpoint is working."))
+
+	// parse the request
+	var loginReq LoginRequest
+	err := json.NewDecoder(r.Body).Decode(&loginReq)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid JSON"))
+		return
+	}
+
+	fmt.Printf("Username: %s, Password: %s\n", loginReq.Username, loginReq.Password)
+	w.Write([]byte(fmt.Sprintf("Received user: %s", loginReq.Username)))
 }
