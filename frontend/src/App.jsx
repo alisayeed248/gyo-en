7ac4bc5be91265bb113678ba080fae71a7820846
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-06-26 22:27:53 -0400

    Added functionality so Navbar will know if user is logged in.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Login from "./components/Login";
 
 function App() {
+  // check t o see if we're logged in when going on the website
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwt_token");
+    const savedUsername = localStorage.getItem("username");
+    if (token) {
+      setIsLoggedIn(true);
+      setUsername(savedUsername || "");
+      console.log("User already logged in:", savedUsername);
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       {/* We keep Navbar at the top because we want it to show on every page */}
-      <Navbar/>
+      <Navbar isLoggedIn={isLoggedIn} username={username}/>
       {/** The routes allow us to say: based on this path, effect this prop (element) */}
       <Routes>
         <Route path="/" element={<Dashboard />}></Route>
