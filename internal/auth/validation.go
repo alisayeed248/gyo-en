d2eebba8c1e3abbf75f2bf1c326c87c8b57a2d8a
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-06-28 20:04:58 -0400

    Added function to take a JWT string and return user info (claims) if valid.

--- internal/auth/validation.go
+++ internal/auth/validation.go
@@ -60,3 +60,20 @@ func GenerateJWT(userID uint, username string) (string, error) {
 
 	return tokenString, nil
 }
+
+func validateJWT(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("invalid token")
+
+}
